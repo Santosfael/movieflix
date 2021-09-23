fed2eef8b646ffb600769f3a032c61c60387051f
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-09-23 11:08:11 -0300

    Crud Review

--- backend/src/main/java/com/rafaelrocha/backend/resources/ReviewResource.java
+++ backend/src/main/java/com/rafaelrocha/backend/resources/ReviewResource.java
@@ -4,14 +4,12 @@ import com.rafaelrocha.backend.dto.ReviewDTO;
 import com.rafaelrocha.backend.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/reviews")
@@ -20,11 +18,40 @@ public class ReviewResource {
     @Autowired
     private ReviewService reviewService;
 
+    @GetMapping
+    public ResponseEntity<List<ReviewDTO>> findAll() {
+        List<ReviewDTO> reviewDTOs = reviewService.findAll();
+
+        return ResponseEntity.ok().body(reviewDTOs);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ReviewDTO> findById(@PathVariable Long id) {
+        ReviewDTO reviewDTO = reviewService.findById(id);
+
+        return ResponseEntity.ok().body(reviewDTO);
+    }
+
     @PostMapping
     public ResponseEntity<ReviewDTO> insert(@Valid @RequestBody ReviewDTO reviewDTO) {
         reviewDTO = reviewService.insert(reviewDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
                 .buildAndExpand(reviewDTO.getId()).toUri();
+
         return ResponseEntity.created(uri).body(reviewDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<ReviewDTO> update(@PathVariable Long id, @Valid @RequestBody ReviewDTO reviewDTO) {
+        reviewDTO = reviewService.update(id, reviewDTO);
+
+        return ResponseEntity.ok().body(reviewDTO);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> dalete(@PathVariable Long id) {
+        reviewService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/com/rafaelrocha/backend/services/ReviewService.java
+++ backend/src/main/java/com/rafaelrocha/backend/services/ReviewService.java
@@ -7,11 +7,20 @@ import com.rafaelrocha.backend.entities.User;
 import com.rafaelrocha.backend.repositories.MovieRepository;
 import com.rafaelrocha.backend.repositories.ReviewRepository;
 import com.rafaelrocha.backend.repositories.UserRepository;
+import com.rafaelrocha.backend.services.exceptions.DataBaseException;
+import com.rafaelrocha.backend.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ReviewService {
@@ -24,6 +33,19 @@ public class ReviewService {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    @Transactional(readOnly = true)
+    public List<ReviewDTO> findAll() {
+        List<Review> reviews = reviewRepository.findAll();
+        return reviews.stream().map(ReviewDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewDTO findById(Long id) {
+        Optional<Review> reviewObj = reviewRepository.findById(id);
+        Review review = reviewObj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ReviewDTO(review);
+    }
 
     @Transactional
     public ReviewDTO insert(ReviewDTO reviewDTO) {
@@ -42,5 +64,34 @@ public class ReviewService {
 
         return new ReviewDTO(review);
 
+    }
+
+    @Transactional
+    public ReviewDTO update(Long id, ReviewDTO reviewDTO) {
+        try {
+            Review review = reviewRepository.getOne(id);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = userRepository.findByEmail(authentication.getName());
+
+            Movie movie = movieRepository.getOne(reviewDTO.getMovieId());
+
+            review.setUser(user);
+            review.setText(review.getText());
+            review.setMovie(movie);
+
+            return new ReviewDTO(review);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            reviewRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation");
+        }
     }
 }
